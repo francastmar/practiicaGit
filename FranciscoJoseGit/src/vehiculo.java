4721c02f1927c3861f0af292cae12e53891615da
fran <francastillo2009@hotmail.es>
2021-05-31 18:27:33 +0200

    commit de la clase vehiculo

---
+++ FranciscoJoseGit/src/vehiculo.java
@@ -0,0 +1,13 @@
+
+public class vehiculo {
+	private int numPuertas;
+	private int numRuedas;
+	
+	
+	public vehiculo( int numPuertas, int numRuedas){
+		this. numPuertas = numPuertas;
+		this. numRuedas = numRuedas;
+		}
+
+
+}
